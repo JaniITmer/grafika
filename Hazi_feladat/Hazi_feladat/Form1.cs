761ebfd0a140d478904a98a12468fb4e8ba29220
JaniITmer <batyus4563@gmail.com>
2024-09-20 20:39:46 +0200

    modositas

--- Hazi_feladat/Hazi_feladat/Form1.cs
+++ Hazi_feladat/Hazi_feladat/Form1.cs
@@ -40,14 +40,29 @@ namespace Hazi_feladat
             g.FillRectangle(new SolidBrush(Color.DarkGreen), 120, 250, 22, 160);
 
 
-
+             
             g.FillRectangle(new SolidBrush(Color.RosyBrown), 200, 150, 240, 240);
             g.DrawRectangle(new Pen(Color.Black, 5f), 230, 260, 70, 70);
             g.FillRectangle(new SolidBrush(Color.LightBlue), 230, 260, 70, 70);
             g.DrawRectangle(new Pen(Color.Black, 3f), 330, 260,70, 130);
             g.FillRectangle(new SolidBrush(Color.SaddleBrown), 330, 260, 70, 130);
-            g.DrawEllipse(new Pen(Color.Orange, 3f), 600, 50, 100, 100);
-            g.FillEllipse(new SolidBrush(Color.Yellow), 600, 50, 100, 100);
+            g.DrawEllipse(new Pen(Color.Orange, 3f), 500, 30, 100, 100);
+            g.FillEllipse(new SolidBrush(Color.Yellow), 500, 30, 100, 100);
+
+            g.DrawLine(new Pen(Color.Black, 14f), 600, 400 , 640, 300 );
+            g.DrawLine(new Pen(Color.Black, 14f), 640, 300, 680, 400);
+            g.DrawLine(new Pen(Color.Black, 14f), 640, 300, 640, 200);
+            g.DrawLine(new Pen(Color.Black, 14f), 640, 200, 700, 300);
+            g.DrawLine(new Pen(Color.Black, 14f), 640, 200, 580, 300);
+            g.FillEllipse(new SolidBrush(Color.Black), 615, 130, 50, 70);
+
+            Point[] points = { new Point(170,160), new Point(330,60), new Point(470,160) };
+            g.FillPolygon(new SolidBrush(Color.Red), points);
+
+
+          
+
+
         }
     }
 }
